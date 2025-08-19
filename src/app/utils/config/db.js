e22ef23c7dcb409c50a66319d2f26573f36204a5
seshu <chaitumulagani@gmail.com>
2025-08-19 14:43:53 +0530

    db.js is changed

--- src/app/utils/config/db.js
+++ src/app/utils/config/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
+import 'dotenv/config'; // Load environment variables
 
-export const DBconnection = async ()=> {
-
+export const DBconnection = async () => {
     try {
-        await mongoose.connect(process.env.THE_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB is Connected");
-        
     } catch (error) {
-        console.log("DB is Not Connected",error);
-        
+        console.log("DB is Not Connected", error);
     }
 }
